cfdd5fe545c916407a8aa3e09cfb1912c6426544
Yogita <yogitapandit2505@gmail.com>
2026-09-07 13:55:47 +0530

    Removed dheeraj

--- src/app/components/data/DesignTeam.jsx
+++ src/app/components/data/DesignTeam.jsx
@@ -1,46 +1,31 @@
 const DesignTeam = [
-    {
-      image: "/TeamImgs/design/aditya.jpg",
-      name: "Aditya Jain",
-      designation: "Coordinator",
-       socials: {
-        linkedin: "https://www.linkedin.com/in//",
-        instagram: "https://www.instagram.com/aadidj.jain/",
-      },
-     
+  {
+    image: "/TeamImgs/design/aditya.jpg",
+    name: "Aditya Jain",
+    designation: "Coordinator",
+    socials: {
+      linkedin: "https://www.linkedin.com/in//",
+      instagram: "https://www.instagram.com/aadidj.jain/",
     },
-    {
-      image: "/TeamImgs/design/dheeraj.jpg",
-      name: "Dheeraj Rajput",
-      designation: "Coordinator",
-       socials: {
-        linkedin: "https://www.linkedin.com/in//",
-        instagram: "https://www.instagram.com/dheerajrajput6757/",
-      },
-      
+  },
+  {
+    image: "/TeamImgs/design/keshav.jpg",
+    name: "Keshav",
+    designation: "Coordinator",
+    socials: {
+      linkedin: "https://www.linkedin.com/in//",
+      instagram: "https://www.instagram.com/keshav_bhardwaj.1?igsh=N212OGV5ZDNyMDdy",
     },
-    {
-      image: "/TeamImgs/design/keshav.jpg",
-      name: "Keshav",
-      designation: "Coordinator",
-       socials: {
-        linkedin: "https://www.linkedin.com/in//",
-        instagram: "https://www.instagram.com/keshav_bhardwaj.1?igsh=N212OGV5ZDNyMDdy",
-      },
-    
+  },
+  {
+    image: "/TeamImgs/design/yash.jpg",
+    name: "Yash Tohan",
+    designation: "Coordinator",
+    socials: {
+      linkedin: "https://www.linkedin.com/in//",
+      instagram: "https://www.instagram.com/neucrodite/",
     },
-    {
-      image: "/TeamImgs/design/yash.jpg",
-      name: "Yash Tohan",
-      designation: "Coordinator",
-       socials: {
-        linkedin: "https://www.linkedin.com/in//",
-        instagram: "https://www.instagram.com/neucrodite/",
-      },
-     
-    },
-    
-  ];
-  
-  export default DesignTeam;
-  
+  },
+];
+
+export default DesignTeam;
